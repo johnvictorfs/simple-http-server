e496d5ab5f5bb0f1a725ce4c513f1c664946244b
João Victor <johnvictorfs@gmail.com>
2019-10-19 03:20:42 -0300

    Add return type hints to routes.

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
+from typing import List, Dict
+
 from server import Server
 
 server = Server()
 
 
 @server.route('users')
-def users():
+def users() -> List[Dict[str, str]]:
     return [{'username': 'NRiver'}, {'username': 'Potato'}]
 
 
 @server.route('posts')
-def posts():
+def posts() -> List[Dict[str, str]]:
     return [{'title': 'Potato Title'}, {'title': 'Cenoura Title'}]
 
 
